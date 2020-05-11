1d51e03385f52f4af3deecc18e47964f662ef1c3
Sabrina Becker <sbecka8@gmail.com>
2020-05-11 16:16:37 -0400

    Working with compose and pipe from lodash

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import { compose, pipe } from 'lodash/fp';
 
 let input = ' JS ';
 const trim = str => str.trim();
+const toLowerCase = str => str.toLowerCase();
 const wrapInDiv = str => `<div>${str}</div>`;
+// const result = wrapInDiv(toLowerCase(trim(input)));
 
-const result = wrapInDiv(trim(input));
+// compose is higher order function
+// const transform = compose(wrapInDiv, toLowerCase, trim);
+
+// pipe helps read left to right
+const transform = pipe(trim, toLowerCase, wrapInDiv);
+
+transform(input);
